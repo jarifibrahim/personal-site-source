16111a7b3023f04a89505564ba537be84a1d3082
Ibrahim Jarif <jarifibrahim@gmail.com>
2019-01-28 00:23:23 +0530

    Commit updated submodule in main project after pushing it to Github

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,16 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
+echo -e "\033[0;32mNew content pushed to Github\033[0m"
 # Come Back up to the Project Root
 cd ..
+
+echo -e "\033[0;32mUpdating public submodule...\033[0m"
+
+# Update the public submodule
+git add public
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
